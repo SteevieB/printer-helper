1fe83932a599b4bee6c1cef3649fe009cf38cb23
Stefan 'SteevieB' Brandl <steevie.brandl@gmail.com>
2021-04-27 12:50:35 +0200

    removed comment, arbitrary path

--- print_all_pdfs.ps1
+++ print_all_pdfs.ps1
@@ -1,13 +1,12 @@
-$Folder = "C:\Users\Stefan\Desktop\Neuer Ordner"
+$Folder = "."
 $FilesToPrint = Get-ChildItem $Folder -Recurse -Include *.pdf
 
-echo "Ordner für gedruckte Dateien wird erstellt"
+echo "Ordner fï¿½r gedruckte Dateien wird erstellt"
 md -Force $Folder\gedruckt
 echo "Beginne mit dem Ausdrucken..."
 
-foreach($File in $FilesToPrint)
-{
-    Start-Process -FilePath $File.FullName -Verb Print -PassThru | %{ sleep 10;$_ } | kill
+foreach ($File in $FilesToPrint) {
+    Start-Process -FilePath $File.FullName -Verb Print -PassThru | % { sleep 10; $_ } | kill
 }
 echo "gedruckte .pdf-Dateien werden verschoben..."
 mv *.pdf $Folder\gedruckt\
